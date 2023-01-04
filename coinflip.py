d39e26499c2b507146b06486a38972a41ca52fcd
tinyfro <adamrzu@gmail.com>
2023-01-04 23:08:13 +1100

    Added comments advising of user request as per the project documentation.

--- coinflip.py
+++ coinflip.py
@@ -1,1 +1,5 @@
+# As a user I want to be able to guess the outcome of a random coin flip(heads/tails)
+# As a user I want to clearly see the result of the coin flip.
+# As a user I want to clearly see whether or not I guessed correctly.
+
 
