5711d520e657233c1599c817dfee3c0d0a6bf40d
tinyfro <adamrzu@gmail.com>
2023-01-04 23:11:09 +1100

    Added link to the webpage with info for the project.

--- coinflip.py
+++ coinflip.py
@@ -1,3 +1,5 @@
+# Project information: https://discuss.codecademy.com/t/computer-science-independent-project-1-coin-flip/419951?_gl=1*1hvpca4*_ga*NzE2MTE1NjM5Ny4xNjcyMzY3MDk3*_ga_3LRZM6TM9L*MTY3MjgyMzUyNC4xMC4xLjE2NzI4MzAzODguNjAuMC4w
+
 # As a user I want to be able to guess the outcome of a random coin flip(heads/tails)
 # As a user I want to clearly see the result of the coin flip.
 # As a user I want to clearly see whether or not I guessed correctly.
