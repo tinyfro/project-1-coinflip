790fd4261622e4eada29670af64c34468c19bfbe
tinyfro <adamrzu@gmail.com>
2023-01-09 19:27:11 +1100

    Added dice roll game, main menu, working on scoring.

--- coinflip.py
+++ coinflip.py
@@ -14,19 +14,31 @@
 
 import random
 
-def coin_flip():
-    guess = guess_coin_side()
-    answers = ["Heads", "Tails"]
-    rand_flip = random.choice(answers)
-    if (rand_flip == "Heads" and guess == "Tails") or (rand_flip == "Tails" and guess == "Heads"):
-        print(guess + ", was the incorrect guess! Please try again.")
-    else:
-        print(guess + ", was the correct answer!")
-    coin_flip()
+dice_roll_score = []
+coin_flip_score = []
+
+def main():
+    main_menu()
+
+def main_menu():
+    while True:
+        choice = input("Welcome to the games menu. Please choose a number to make a choice:\n1. Play Coinflip\n2. View Coinflip scores\n3. Play Diceroll\n4. View Diceroll scores\n")
+        if choice.isdigit():
+            choice = int(choice)
+            if choice == 1:
+                coin_flip()
+            elif choice == 2:
+                pass
+            elif choice == 3:
+                dice_roll()
+            elif choice == 4:
+                pass
+            else:
+                print("To make a choice, please choose the corresponding number.")
 
 def guess_coin_side():
     while True:
-        guess = input("I flipped a coin, enter 'Heads' or 'Tails' to guess which side it landed on: \n")
+        guess = input("I flipped a coin, enter 'Heads' or 'Tails' to guess which side it landed on: ")
         if isinstance(guess, str):
             guess = str(guess)
             if guess == "Heads" or guess == "Tails" or guess == "heads" or guess == "tails":
@@ -37,14 +49,41 @@ def guess_coin_side():
             print("You must enter 'Heads' or 'Tails'. Try again.")
     return str.capitalize(guess)
 
-def main_menu():
+def guess_dice_roll():
     while True:
-        choice = input("Welcome to this game. Please type the corresponding number to make a play choice:\n1. Coinflip\n2. View coinflip scores\n3. Play 'dice roll'\n4. View 'dice roll' scores\n")
-        if choice.isdigit():
-            choice = int(choice)
-            break
+        guess = input("I rolled a dice, enter the number that the dice rolled to: ")
+        if guess.isdigit():
+            guess = int(guess)
+            if guess > 0 and guess <= 6:
+                break
+            else:
+                print("You must enter a number from 1 to 6. Try again.")
+        else:
+            print("You must enter a number from 1 to 6. Try again.")
+    return guess
 
-def main():
-    main_menu()
+def coin_flip():
+    guess = guess_coin_side()
+    answers = ["Heads", "Tails"]
+    rand_flip = random.choice(answers)
+    if (rand_flip == "Heads" and guess == "Tails") or (rand_flip == "Tails" and guess == "Heads"):
+        print(guess + ", was the incorrect guess! Please try again.")
+    else:
+        print(guess + ", was the correct guess!")
+    coin_flip()
+
+def dice_roll():
+    numerical_score = 0
+    guess = guess_dice_roll()
+    answer = random.randint(1, 6)
+    if answer == guess:
+        #dice_roll_score.append([guess, numerical_score])
+        print(str(guess) + ", was the correct guess!")
+    else:
+        print(str(guess) + ", was the incorrect guess! Please try again.")
+    dice_roll()
+
+def display_scores():
+    pass
 
 main()
