5aec6d60458eeaeadc2c9e7347fd361ba0a9d15e
tinyfro <adamrzu@gmail.com>
2023-01-15 17:57:09 +1100

    Completed project - may be bugs still though

--- coinflip.py
+++ coinflip.py
@@ -1,43 +1,57 @@
 # Project information: https://discuss.codecademy.com/t/computer-science-independent-project-1-coin-flip/419951?_gl=1*1hvpca4*_ga*NzE2MTE1NjM5Ny4xNjcyMzY3MDk3*_ga_3LRZM6TM9L*MTY3MjgyMzUyNC4xMC4xLjE2NzI4MzAzODguNjAuMC4w
 
 # Basic requirements:
-# As a user I want to be able to guess the outcome of a random coin flip(heads/tails)
-# As a user I want to clearly see the result of the coin flip.
-# As a user I want to clearly see whether or not I guessed correctly.
+# As a user I want to be able to guess the outcome of a random coin flip(heads/tails) DONE
+# As a user I want to clearly see the result of the coin flip. DONE
+# As a user I want to clearly see whether or not I guessed correctly. DONE
 
 # Intermediate challenges:
-# User Story: As a user I want to clearly see the updated guess history (correct count/total count).
+# User Story: As a user I want to clearly see the updated guess history (correct count/total count). DONE
 # User Story: As a user I want to be able to quit the game or go again after each cycle.
 
 # Advanced challenge:
 # User Story: As a user I want to be able to guess the outcome of a 6-sided dice roll (1-6), with the same feature set as the coin flip (see above).
 
-import random
+import random, os
 
-dice_roll_score = []
-coin_flip_score = []
+scores = {"coin_flip": [], "dice_roll": []}
 
 def main():
+    """Main function that runs the program"""
+    scores["coin_flip_total"] = 0
+    scores["dice_roll_total"] = 0
     main_menu()
 
+def clear_cns():
+    return os.system("cls" if os.name == "nt" else "clear")
+
 def main_menu():
+    """Displays the main menu with options for the user to choose from"""
     while True:
-        choice = input("Welcome to the games menu. Please choose a number to make a choice:\n1. Play Coinflip\n2. View Coinflip scores\n3. Play Diceroll\n4. View Diceroll scores\n")
+        clear_cns()
+        choice = input("Welcome to the games menu. Please choose a number to make a choice:\n1. Play Coinflip\n2. View Coinflip scores\n3. Play Diceroll\n4. View Diceroll scores\n5. Exit\n")
         if choice.isdigit():
             choice = int(choice)
             if choice == 1:
+                clear_cns()
                 coin_flip()
             elif choice == 2:
-                pass
+                clear_cns()
+                display_scores("coin_flip")
             elif choice == 3:
                 dice_roll()
             elif choice == 4:
-                pass
+                clear_cns()
+                display_scores("dice_roll")
+            elif choice == 5:
+                exit()
             else:
                 print("To make a choice, please choose the corresponding number.")
 
 def guess_coin_side():
+    """Asks the user to guess which side of a coin landed facing up"""
     while True:
+        clear_cns()
         guess = input("I flipped a coin, enter 'Heads' or 'Tails' to guess which side it landed on: ")
         if isinstance(guess, str):
             guess = str(guess)
@@ -50,7 +64,9 @@ def guess_coin_side():
     return str.capitalize(guess)
 
 def guess_dice_roll():
+    """Asks the user to guess the number that a dice rolled"""
     while True:
+        clear_cns()
         guess = input("I rolled a dice, enter the number that the dice rolled to: ")
         if guess.isdigit():
             guess = int(guess)
@@ -63,27 +79,45 @@ def guess_dice_roll():
     return guess
 
 def coin_flip():
-    guess = guess_coin_side()
-    answers = ["Heads", "Tails"]
-    rand_flip = random.choice(answers)
-    if (rand_flip == "Heads" and guess == "Tails") or (rand_flip == "Tails" and guess == "Heads"):
-        print(guess + ", was the incorrect guess! Please try again.")
-    else:
-        print(guess + ", was the correct guess!")
-    coin_flip()
+    """Flips a coin and keeps track of the user's score"""
+    while True:
+        guess = guess_coin_side()
+        answers = ["Heads", "Tails"]
+        rand_flip = random.choice(answers)
+        if (rand_flip == "Heads" and guess == "Tails") or (rand_flip == "Tails" and guess == "Heads"):
+            print(guess + ", was the incorrect guess! Please try again.")
+            scores["coin_flip_total"] += 1
+        else:
+            print(guess + ", was the correct guess!")
+            scores["coin_flip"].append(1)
+            scores["coin_flip_total"] += 1
+        if input("Do you want to continue playing? (y/n) ").lower() == 'n':
+            break
 
 def dice_roll():
-    numerical_score = 0
-    guess = guess_dice_roll()
-    answer = random.randint(1, 6)
-    if answer == guess:
-        #dice_roll_score.append([guess, numerical_score])
-        print(str(guess) + ", was the correct guess!")
-    else:
-        print(str(guess) + ", was the incorrect guess! Please try again.")
-    dice_roll()
+    """Rolls a dice and keeps track of the user's score"""
+    while True:
+        guess = guess_dice_roll()
+        answer = random.randint(1, 6)
+        if answer == guess:
+            scores["dice_roll"].append(1)
+            scores["dice_roll_total"] += 1
+            print(str(guess) + ", was the correct guess!\n")
+        else:
+            scores["dice_roll_total"] += 1
+            print(str(guess) + ", was the incorrect guess! Please try again.\n")
+        if input("Do you want to continue playing? (y/n) ").lower() == 'n':
+            break
 
-def display_scores():
-    pass
+def display_scores(game):
+    """Displays the user's scores for a specific game"""
+    if game in scores:
+        if scores[game]:
+            print("Your score for {} is: {}/{} (Correct Guesses / Total Guesses)".format(game.title().replace("_", " "), sum(scores[game]), scores[game + "_total"]))
+        else:
+            print("You haven't played {} yet.\n".format(game.title().replace("_", " ")))
+    else:
+        print("Invalid game. Please choose a valid game.\n")
+    input("Press Enter to go to the main menu...")
 
 main()
